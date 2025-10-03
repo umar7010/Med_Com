0c42c419147a198531452422da7b9b089bd3036e
IT24101134 <it24101134@my.sliit.lk>
2025-10-03 18:28:30 +0530

    initial commit with database connection

---
+++ src/main/java/com/medicine/site/CompleteMedSiteApplication.java
@@ -0,0 +1,13 @@
+package com.medicine.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteMedSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteMedSiteApplication.class, args);
+    }
+
+}
